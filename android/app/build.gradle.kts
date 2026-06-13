3de0b10bb78b6b8cd6e29beb77b9e6a83fb1b4c9
Ibrahim Sakho <ibrahimsakho@Ibrahims-MacBook-Air.local>
2026-06-13 14:46:29 +0000

    UPDATED THE BUNDLE ID

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.africanmovies"
+    namespace = "com.africanmovies.mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.africanmovies"
+        applicationId = "com.africanmovies.mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/africanmovies/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/africanmovies/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.africanmovies
+package com.africanmovies.mobile
 
 import io.flutter.embedding.android.FlutterActivity
 
